21c6f17599e7bf74d6850dea9614b1ea69dfcb21
Alena Lychko <alenaxxprv@gmail.com>
2025-07-18 13:02:19 -0400

    ы

---
+++ main-2.js
@@ -0,0 +1,53 @@
+// ex. 1. 
+let celsius = 25;
+let fahrenheit = (celsius * 9) / 5 + 32;
+console.log("Температура в Фаренгейтах:", fahrenheit);
+
+// ex. 2. 
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+console.log("Годин у місяці:", hoursInMonth);
+console.log("Хвилин у місяці:", minutesInMonth);
+
+//  ex. 3. 
+let health = 100;
+let energy = 80;
+health = 20;
+energy = 30;
+console.log("Здоров’я після атаки:", health);
+console.log("Енергія після атаки:", energy);
+
+//  ex. 4. 
+let totalPrice = 200;
+let discount = 10; 
+let discountedPrice = totalPrice * (1 - discount);
+console.log("Ціна зі знижкою:", discountedPrice); 
+
+//  ex. 5. 
+const floatNumber = 12.78;
+let roundedDown = Math.floor(floatNumber);
+console.log("Округлене до меншого:", roundedDown);
+
+//  ex. 6. 
+const floatString = "45.67";
+let parsedFloat = parseFloat(floatString);
+console.log("Перетворено в десяткове число:", parsedFloat);
+
+//  ex. 7. 
+const intString = "123";
+let parsedInt = parseInt(intString);
+console.log("Перетворено в ціле число:", parsedInt);
+
+//  ex. 8.
+let number = 64;
+let sqrtNumber = Math.sqrt(number);
+console.log("Квадратний корінь з", number, ":", sqrtNumber);
+
+//  ex. 9.
+const integer = 42;
+const stringNumber = "256";
+let convertedInt = parseInt(stringNumber);
+let convertedString = integer.toString();
+console.log("Рядок '256' в число:", convertedInt);
+console.log("Число 42 в рядок:", convertedString);
