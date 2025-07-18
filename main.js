34a37eace87806311b23eadb555b059cc3c863ef
Alena Lychko <alenaxxprv@gmail.com>
2025-07-18 12:55:03 -0400

    d

---
+++ main.js
@@ -0,0 +1,30 @@
+//first ex.
+let age = 15;
+console.log(age);
+//second ex.
+let name = "Alena";
+console.log(name);
+//third ex.
+let isStudent = true;
+console.log(isStudent);
+//fourth ex.
+let myString =
+  "Чому вони 5ть років минулих руйнували краЇну, ми Їм не заважали, ми Їм підсказували! -Виктор Янукович";
+console.log(myString);
+//fifth ex.
+let myNumber = 1707.08;
+myNumber = myNumber + 10;
+console.log(myNumber);
+//sixth ex.
+let myNull = null;
+console.log(myNull);
+//seventh ex.
+let userName = prompt("Введіть своє ім’я");
+alert("Ваше ім’я: " + userName);
+console.log("Тип змінної userName:", typeof userName);
+//eighth ex.
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+//nineth ex.
+alert("Увага! Дія може бути небезпечною.");
+confirm("Ви впевнені, що хочете продовжити?");
