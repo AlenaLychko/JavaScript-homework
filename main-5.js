906d0c94bf57afacfb627686a00d8f8a2675d4a5
Alena Lychko <alenaxxprv@gmail.com>
2025-07-30 16:37:53 -0400

    iu

---
+++ main-5.js
@@ -0,0 +1,100 @@
+//ex 1
+const drinks = "coffee";
+switch (drinks) {
+  case "coffee":
+    console.log("your choice is: coffee. Enjoy!");
+    break;
+  case "tea":
+    console.log("your choice is: tea. Enjoy!");
+  case "juice":
+    console.log("your choice is: juice. Enjoy!");
+    break;
+
+}
+
+//ex 2
+
+ const week = "Monday";
+
+ switch (week) {
+   case "Monday":
+     console.log("go to work, it's a weekday");
+     break;
+   case "Tuesday":
+     console.log("go to work, it's a weekday");
+     break;
+   case "Wednesday":
+     console.log("go to work, it's a weekday");
+     break;
+   case "Thursday":
+     console.log("go to work, it's a weekday");
+     break;
+   case "Friday":
+     console.log("go to work, it's a weekday");
+     break;
+   case "Saturday":
+     console.log("you can relax this weekend");
+     break;
+   case "Sunday":
+     console.log("you can relax this weekend");
+     break;
+   
+ }
+ //ex 3
+
+ const season = "1";
+
+ switch (season) {
+   case "1":
+     console.log("you chose the first month it's: winter, january ");
+     break;
+   case "2":
+     console.log("you chose the second month it's: winter, february ");
+     break;
+   case "3":
+     console.log("you chose the third month it's: spring, march ");
+     break;
+   case "4":
+     console.log("you chose the fourth month it's: spring, april ");
+     break;
+   case "5":
+     console.log("you chose the fifth month it's: spring, may ");
+     break;
+   case "6":
+     console.log("you chose the sixth month it's: summer, june ");
+     break;
+   case "7":
+     console.log("you chose the seventh month it's: summer, july ");
+     break;
+   case "8":
+     console.log("you chose the eight month it's: summer, august ");
+     break;
+   case "9":
+     console.log("you chose the nineth month it's: autumn, september ");
+     break;
+   case "10":
+     console.log("you chose the tenth t month it's: autumn, october ");
+     break;
+   case "11":
+     console.log("you chose the elenth month it's: autumn, november ");
+     break;
+   case "12":
+     console.log("you chose the twelth month it's: winter, december ");
+     break;
+
+ }
+
+ //ex 4
+ const traffic = "yellow";
+  switch (traffic) {
+    case "green":
+      console.log("you can go!");
+      break;
+    case "yellow":
+      console.log("stop! could be dangerous");
+      break;
+    case "red":
+      console.log("stop!");
+      break;
+  }
+  //ex 5
