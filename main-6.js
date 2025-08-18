5887275a617310cab5b69693a0fb6f5cac32cb99
Alena Lychko <alenaxxprv@gmail.com>
2025-08-17 22:24:36 -0400

    ы

---
+++ main-6.js
@@ -0,0 +1,24 @@
+//1
+let nums = 1;
+while (nums <= 10) {
+  console.log(nums);
+  nums++;
+}
+//2
+for (let j = 2; j <= 20; j++) {
+  if (j % 2 !== 0) {
+    continue;
+  }
+  console.log(j);
+}
+
+//3
+//4
+let n = 9;
+for (let p = 6; p <= 20; p++) {
+  if (p >= n) {
+    console.log(p);
+    break;
+  }
+}
+//5 шооооо це капець
