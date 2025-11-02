030dafebff0deb2acc20ade9423e8d5cc48df42c
Alena Lychko <alenaxxprv@gmail.com>
2025-11-02 17:48:33 -0500

    d

--- main-12.js
+++ main-12.js
@@ -16,3 +16,49 @@ const user = {
 
  //ex 2
  //ex 3
+ function findBestEmployee(employees) {
+   let bestEmployee = "";
+   let maxTasks = 0;
+
+   for (const [name, tasks] of Object.entries(employees)) {
+     if (tasks > maxTasks) {
+       maxTasks = tasks;
+       bestEmployee = name;
+     }
+   }
+
+   return bestEmployee;
+ }
+
+ //ex4
+ function countTotalSalary(employees) {
+  return Object.values(employees).reduce((total, salary) => total + salary, 0);
+}
+
+const employees = {
+  Daria: 6700,
+  Jose: 5500,
+  Danielle: 4589,
+};
+
+console.log(countTotalSalary(employees)); 
+
+//ex5
+//ex6
+function calculateTotalPrice(allProducts, productName) {
+  let total = 0;
+
+  for (const product of allProducts) {
+    if (product.name === productName) {
+      total = product.price * product.quantity;
+      break;
+    }
+  }
+
+  return total;
+}
+
+
+console.log(calculateTotalPrice(products, "cat")); 
+console.log(calculateTotalPrice(products, "dog")); 
+console.log(calculateTotalPrice(products, "horse"));  
