963bf26396983283b9753ff6c02947ab15d0c798
Alena Lychko <alenaxxprv@gmail.com>
2025-11-15 12:05:39 -0500

    в

--- main18.js
+++ main18.js
@@ -1,12 +1,34 @@
-//ex 1
-const categoriesList = document.querySelector("categories");
-const categoryItems = categoriesList.querySelectorAll(".item");
+// //ex 1
+// const categoriesList = document.querySelector("#categories");
+// const categoryItems = categoriesList.querySelectorAll("ul");
 
-console.log(`there is ${categoriesItems.lenght} categories`);
+// console.log(`in list ${categoryItems.length} categories.`);
 
-categorieItems.forEach((item) => {
-  const title = item.querySelector("h2").textContent;
-  const elementCount = item.querySelectorAll('ul li').lenght
-  console.log(`Категорія: ${title}`);
-  console.log(`Кількість елементів: ${elementsCount}`);
+// categoryItems.forEach((category) => {
+//   const title = category.previousElementSibling.textContent;
+//   const elementsCount = category.querySelectorAll("li").length;
+
+//   console.log(`category: ${title}`);
+//   console.log(`amount of categories: ${elementsCount}`);
+// });
+
+//ex 2
+const ingredients = [
+  "Картопля",
+  "Гриби",
+  "Часник",
+  "Помідори",
+  "Зелень",
+  "Приправи",
+];
+
+const ul = document.getElementById("ingredients");
+
+const liElements = ingredients.map((ingredients) => {
+  const li = document.createElement("li");
+  li.textContent = ingredients;
+  return li;
 });
+ul.append(...liElements);
+
+//ex 3
